a09877091391fb3b8e11227f4f382994abb98c5c
racoolzinha <127261651+racoolzinha@users.noreply.github.com>
2026-06-07 15:06:08 -0300

    Add files via upload

---
+++ cadastro/FormCadastro.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace cadastro
+{
+    partial class FormCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.txtSelecao = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.chkObtida = new System.Windows.Forms.CheckBox();
+            this.chkDesejada = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFoto = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(215)))), ((int)(((byte)(75)))));
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(512, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CADASTRE UMA FIGURINHA AO SEU ÁLBUM!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(19, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(371, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quem ou o que está estampado nesta figurinha?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(19, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Qual é o tipo desta figurinha?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(19, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "A qual seleção ela pertence?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(670, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "STATUS:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(49)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.btnSalvar);
+            this.panel1.Controls.Add(this.cmbTipo);
+            this.panel1.Controls.Add(this.txtSelecao);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(35, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(535, 412);
+            this.panel1.TabIndex = 5;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(18)))), ((int)(((byte)(119)))));
+            this.btnSalvar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalvar.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalvar.Location = new System.Drawing.Point(356, 334);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(157, 48);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Criar Figurinha";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Comum",
+            "Rara"});
+            this.cmbTipo.Location = new System.Drawing.Point(23, 263);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(213, 21);
+            this.cmbTipo.TabIndex = 7;
+            // 
+            // txtSelecao
+            // 
+            this.txtSelecao.Location = new System.Drawing.Point(23, 178);
+            this.txtSelecao.Name = "txtSelecao";
+            this.txtSelecao.Size = new System.Drawing.Size(358, 20);
+            this.txtSelecao.TabIndex = 6;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(23, 104);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(358, 20);
+            this.txtNome.TabIndex = 4;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // chkObtida
+            // 
+            this.chkObtida.AutoSize = true;
+            this.chkObtida.BackColor = System.Drawing.Color.Transparent;
+            this.chkObtida.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkObtida.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.chkObtida.Location = new System.Drawing.Point(672, 391);
+            this.chkObtida.Name = "chkObtida";
+            this.chkObtida.Size = new System.Drawing.Size(78, 25);
+            this.chkObtida.TabIndex = 6;
+            this.chkObtida.Text = "Obtida";
+            this.chkObtida.UseVisualStyleBackColor = false;
+            // 
+            // chkDesejada
+            // 
+            this.chkDesejada.AutoSize = true;
+            this.chkDesejada.BackColor = System.Drawing.Color.Transparent;
+            this.chkDesejada.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkDesejada.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.chkDesejada.Location = new System.Drawing.Point(651, 361);
+            this.chkDesejada.Name = "chkDesejada";
+            this.chkDesejada.Size = new System.Drawing.Size(99, 25);
+            this.chkDesejada.TabIndex = 7;
+            this.chkDesejada.Text = "Desejada";
+            this.chkDesejada.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(215)))), ((int)(((byte)(75)))));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(588, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 190);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFoto
+            // 
+            this.btnFoto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(18)))), ((int)(((byte)(119)))));
+            this.btnFoto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFoto.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFoto.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoto.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnFoto.Location = new System.Drawing.Point(591, 241);
+            this.btnFoto.Name = "btnFoto";
+            this.btnFoto.Size = new System.Drawing.Size(157, 48);
+            this.btnFoto.TabIndex = 9;
+            this.btnFoto.Text = "Adicionar Foto";
+            this.btnFoto.UseVisualStyleBackColor = false;
+            this.btnFoto.Click += new System.EventHandler(this.btnFoto_Click);
+            // 
+            // FormCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::cadastro.Properties.Resources.WhatsApp_Image_2026_05_28_at_8_50_25_PM;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(780, 498);
+            this.Controls.Add(this.btnFoto);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chkDesejada);
+            this.Controls.Add(this.chkObtida);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Name = "FormCadastro";
+            this.Text = "FormCadastro";
+            this.Load += new System.EventHandler(this.FormCadastro_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.TextBox txtSelecao;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.CheckBox chkObtida;
+        private System.Windows.Forms.CheckBox chkDesejada;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnFoto;
+    }
+}
---
+++ cadastro/FormCadastro.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace cadastro
+{
+    public partial class FormCadastro : Form
+    {
+        string caminhoFoto = "";
+
+        public FormCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+        //salvar foto
+                    private void btnFoto_Click(object sender, EventArgs e)
+                    {
+
+
+                            OpenFileDialog foto = new OpenFileDialog();
+
+                            foto.Filter = "Imagens|*.jpg;*.jpeg;*.png";
+
+                            if (foto.ShowDialog() == DialogResult.OK)
+                            {
+                                caminhoFoto = foto.FileName;
+
+                                MessageBox.Show("Foto selecionada com sucesso!");
+
+                                pictureBox1.Image = Image.FromFile(caminhoFoto);
+                            }
+                     }
+
+                     private void chkObtida_CheckedChanged(object sender, EventArgs e)
+                      {
+
+                      }
+
+                      private void chkDesejada_CheckedChanged(object sender, EventArgs e)
+                      {
+
+                      }
+
+
+                      private void btnSalvar_Click(object sender, EventArgs e)
+                      {
+                            if (string.IsNullOrWhiteSpace(txtNome.Text))
+                            {
+                                MessageBox.Show("Preencha o nome.");
+                                return;
+                            }
+
+                            if (string.IsNullOrWhiteSpace(txtSelecao.Text))
+                            {
+                                MessageBox.Show("Preencha a seleção.");
+                                return;
+                            }
+
+                            if (cmbTipo.SelectedIndex == -1)
+                            {
+                                MessageBox.Show("Selecione o tipo da figurinha.");
+                                return;
+                            }
+
+                            if (!chkObtida.Checked && !chkDesejada.Checked)
+                            {
+                                MessageBox.Show("Marque Obtida ou Desejada.");
+                                return;
+                            }
+
+                            if (caminhoFoto == "")
+                            {
+                                MessageBox.Show("Selecione uma foto.");
+                                return;
+                            }
+
+                            try
+                            {
+                                string linha =
+                                    txtNome.Text + ";" +
+                                    txtSelecao.Text + ";" +
+                                    cmbTipo.Text + ";" +
+                                    chkObtida.Checked + ";" +
+                                    chkDesejada.Checked + ";" +
+                                    caminhoFoto;
+
+                                File.AppendAllText(
+                                    "figurinhas.txt",
+                                    linha + Environment.NewLine);
+
+                                MessageBox.Show("Figurinha cadastrada com sucesso!");
+
+                                txtNome.Clear();
+                                txtSelecao.Clear();
+
+                                cmbTipo.SelectedIndex = -1;
+
+                                chkObtida.Checked = false;
+                                chkDesejada.Checked = false;
+
+                                pictureBox1.Image = null;
+
+                                caminhoFoto = "";
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Erro ao salvar: " + ex.Message);
+                            }
+                       }
+
+                       private void label3_Click(object sender, EventArgs e)
+                       {
+
+                       }
+
+                       private void pictureBox1_Click(object sender, EventArgs e)
+                       {
+
+                       }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Dados gravados no formato:
+        // Nome;Selecao;Tipo;Obtida;Desejada;Foto
+    }
+
+}
